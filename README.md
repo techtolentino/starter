da30664f04cd4c0f3bfb65fb1e6cd9f38d1e73d0
Randy Tolentino <makertolentino@gmail.com>
2016-01-12 03:16:48 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+- install [LiveReload]('https://chrome.google.com/webstore/detail/livereload/jnihajbhpnppcggbcgedagnkighmdlei?hl=en')
+
+- `git clone`
+- `npm install`
+- `bundle install`
+- `gulp`
---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    plumber = require('gulp-plumber'),
+    scsslint = require('gulp-scss-lint'),
+    autoprefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify');
+
+// Scripts task, uglifies Javascript files
+gulp.task('scripts', function(){
+  gulp.src('js/*.js')
+    .pipe(plumber())
+    .pipe(uglify())
+    .pipe(gulp.dest('build/js'));
+});
+
+// Styles task, minifies with sourcemaps
+gulp.task('styles', function(){
+  gulp.src('scss/**/*.scss')
+    .pipe(scsslint())
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(autoprefixer({browsers: 'last 2 versions'}))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('build/css'))
+});
+
+// Watch task, watches changes to Javascript and SCSS
+gulp.task('watch', function(){
+  gulp.watch('js/*.js', ['scripts']);
+  gulp.watch('scss/**/*.scss', ['styles']);
+});
+
+
+gulp.task('default', ['scripts', 'styles', 'webserver', 'watch']);
