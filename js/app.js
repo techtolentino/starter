565e84467c701cdcdd60b0a447a5870b9cce33f0
Rishi Raj <rishiraj824@gmail.com>
2016-10-26 21:02:21 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -46,3 +46,11 @@ document.getElementById('checklist--submit').addEventListener("click", function(
 
     scrollToTop();
 });
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
