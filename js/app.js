d954e6d07f3be6b3d480fb3d1dcf0c1ba32f1486
Siwalik <contact@siwalik.in>
2016-10-27 06:29:41 +0530

    Corrected Doctype declaration typo, text formatted css html js and json files for better readability.

--- js/app.js
+++ js/app.js
@@ -1,19 +1,18 @@
 // store each checklist val
-var timeOut,
-    checkListItems = Array.prototype.slice.call(
+var timeOut
+    , checkListItems = Array.prototype.slice.call(
         document.getElementsByClassName("checklist--item-status-box")
     );
 
 // scroll to top function
 function scrollToTop() {
-    if (document.body.scrollTop!=0 || document.documentElement.scrollTop!=0){
-        window.scrollBy(0,-50);
-        timeOut=setTimeout('scrollToTop()',11);
-    }
-    else clearTimeout(timeOut);
+    if (document.body.scrollTop != 0 || document.documentElement.scrollTop != 0) {
+        window.scrollBy(0, -50);
+        timeOut = setTimeout('scrollToTop()', 11);
+    } else clearTimeout(timeOut);
 }
 
-checkListItems.forEach(function(item){
+checkListItems.forEach(function (item) {
     var checkBox = item.getAttribute("id");
 
     if (!window.localStorage.getItem(checkBox) || window.localStorage.getItem(checkBox) === "incomplete") {
@@ -23,7 +22,7 @@ checkListItems.forEach(function(item){
     }
 
     // saving checkbox to store
-    item.addEventListener("click", function() {
+    item.addEventListener("click", function () {
         if (!window.localStorage.getItem(checkBox) || window.localStorage.getItem(checkBox) === "incomplete") {
             window.localStorage.setItem(checkBox, "done")
             item.classList.add("checklist--item-status-box--DONE");
@@ -35,12 +34,12 @@ checkListItems.forEach(function(item){
 });
 
 // reset form
-document.getElementById('checklist--submit').addEventListener("click", function() {
+document.getElementById('checklist--submit').addEventListener("click", function () {
     var checklist = document.getElementById("checklist");
 
     checklist.reset();
     window.localStorage.clear();
-    checkListItems.forEach(function(item) {
+    checkListItems.forEach(function (item) {
         item.classList.remove("checklist--item-status-box--DONE");
     });
 
